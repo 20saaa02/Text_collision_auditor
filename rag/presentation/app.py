3fc17f2f267aceebc7c009ebc71ba0a048bf91f9
Matvey_Emelkov <471918@edu.itmo.ru>
2025-07-23 21:02:34 +0700

    feat: updated app.py

--- rag/presentation/app.py
+++ rag/presentation/app.py
@@ -10,7 +10,7 @@ from sentence_transformers import SentenceTransformer
 
 from service.LLMService import LLMService
 from service.preprocessingDataService import PreprocessingDataService
-from service.RAGRetriever import RAGRetrieverGlobal
+from service.RAGRetriever import BaseRAGRetriever, RAGRetrieverGlobal
 
 from entity.dataPreproc import splittingTextIntoChunksGet, cleanTextGet, initPreprocessingDataServiceGet
 from entity.dataLLM import splittingChunksIntoFactsGet, initLLMServiceGet, findTopNearestLLMGet, findCollisionsGet
@@ -26,10 +26,13 @@ NOF_NEAREST_CELLS_TO_CHECK = 10 # потестить
 
 # Основной класс запуска RAG
 class AnticollisionMainClass:
-    def __init__(self):
+    def __init__(self, is_outer: bool=False):
         self.LLMService = LLMService(initLLMServiceGet())
         self.PreprocessingDataService = PreprocessingDataService(initPreprocessingDataServiceGet())
-        self.RAGRetrieverGlobal = RAGRetrieverGlobal(initDBGet(db_path=SAVE_DB_FILE_PATH))
+        if is_outer:
+            self.RAGRetriever = RAGRetrieverGlobal(initDBGet(db_path=SAVE_DB_FILE_PATH))
+        else:
+            self.RAGRetriever = BaseRAGRetriever()
         # Инициализация модели для эмбеддингов
         self.embedding_model = SentenceTransformer('all-MiniLM-L6-v2')  # Легкая модель
         # Или для русского языка: 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
@@ -38,32 +41,33 @@ class AnticollisionMainClass:
     def prepareDB(self, getData: prepareDBGet) -> prepareDBResult:
         prepareDBRes = prepareDBResult(error=ErrorClass(False, ""))
         # ПРЕДОБРАБОТКА ТЕКСТА
+        """
         textIntoFactsRes = self.__textIntoFacts(textIntoFactsGet(text=getData.text))
         if textIntoFactsRes.error.isError:
             prepareDBRes.error = textIntoFactsRes.error
             return prepareDBRes
+        """
         # ФАКТЫ В ЭМБЕДДИНГИ
-        factsIntoEmbeddingsRes = self.__factsIntoEmbeddings(factsIntoEmbeddingsGet(textIntoFactsRes.facts))
+        factsIntoEmbeddingsRes = self.factsIntoEmbeddings(factsIntoEmbeddingsGet(getData.text))
         if factsIntoEmbeddingsRes.error.isError:
             prepareDBRes.error = factsIntoEmbeddingsRes.error
             return prepareDBRes
         # ЭМБЕДДИНГИ ЗАПИСЫВАЕМ В БАЗУ
-        loadEmbeddingsDBRes = self.RAGRetrieverGlobal.loadEmbeddingsDB(loadEmbeddingsDBGet(sentence_embeddings=factsIntoEmbeddingsRes.embeddings, sentences=textIntoFactsRes.facts))
+        loadEmbeddingsDBRes = self.RAGRetriever.loadEmbeddingsDB(loadEmbeddingsDBGet(sentence_embeddings=factsIntoEmbeddingsRes.embeddings, sentences=getData.text))
         if loadEmbeddingsDBRes.error.isError:
             prepareDBRes.error = loadEmbeddingsDBRes.error
         return prepareDBRes
-
 
     # ПРОВЕРКА КОЛЛИЗИИ ВОПРОСА С ФАКТАМИ ИЗ БАЗЫ ЗНАНИЙ
     def checkCollisionOne(self, getData: checkCollisionOneGet) -> checkCollisionOneResult:
         checkCollisionOneRes = checkCollisionOneResult(list(), ErrorClass(False, ""))
         # ВОПРОС В ЭМБЕДДИНГ
-        factsIntoEmbeddingsRes = self.__factsIntoEmbeddings(factsIntoEmbeddingsGet([getData.question]))
+        factsIntoEmbeddingsRes = self.factsIntoEmbeddings(factsIntoEmbeddingsGet([getData.question]))
         if factsIntoEmbeddingsRes.error.isError:
             checkCollisionOneRes.error = factsIntoEmbeddingsRes.error
             return checkCollisionOneRes
         # ПОИСК 30 БЛИЖАЙШИХ ПО БАЗЕ ЗНАНИЙ
-        findTopNearestDBRes = self.RAGRetrieverGlobal.findTopNearestDB(findTopNearestDBGet(query_embedding=factsIntoEmbeddingsRes.embeddings[0], k=COUNT_NEAR_FIND_DB,\
+        findTopNearestDBRes = self.RAGRetriever.findTopNearestDB(findTopNearestDBGet(query_embedding=factsIntoEmbeddingsRes.embeddings[0], k=COUNT_NEAR_FIND_DB,\
                                                                                             NofNearestCellsToCheck=NOF_NEAREST_CELLS_TO_CHECK))
         if findTopNearestDBRes.error.isError:
             checkCollisionOneRes.error = findTopNearestDBRes.error
@@ -80,7 +84,6 @@ class AnticollisionMainClass:
             return checkCollisionOneRes
         checkCollisionOneRes.arrCollisions = findCollisionsRes.arrCollisionResult
         return checkCollisionOneRes
-
 
     # ПРЕДОБРАБОТКА ТЕКСТА
     def __textIntoFacts(self, getData: textIntoFactsGet) -> textIntoFactsResult:
@@ -111,20 +114,16 @@ class AnticollisionMainClass:
         print("Полученные факты:", textIntoFactsRes.facts)
 
         return textIntoFactsRes
-    
 
     # Преобразует список предложений в массив эмбеддингов
-    def __factsIntoEmbeddings(self, getData: factsIntoEmbeddingsGet) -> factsIntoEmbeddingsResult:
+    def factsIntoEmbeddings(self, getData: factsIntoEmbeddingsGet) -> factsIntoEmbeddingsResult:
         factsIntoEmbeddingsRes = factsIntoEmbeddingsResult(None, ErrorClass(False, ""))
         try:
+            #print(getData.sentences)
             factsIntoEmbeddingsRes.embeddings = self.embedding_model.encode(getData.sentences)
         except Exception as e:
             factsIntoEmbeddingsRes.error = ErrorClass(True, f"Ошибка генерации эмбеддингов: {str(e)}")
         return factsIntoEmbeddingsRes
-
-
-
-
 
 
 if __name__ == "__main__":
